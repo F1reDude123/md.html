7a84a12e54af45827cc482e3cb4de13e2e41a49b
FireDude123 <153016828+F1reDude123@users.noreply.github.com>
2025-11-03 22:18:54 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,40 @@
+class MDReader {
+  readAsHTML(text) {
+    var activeB=false;
+    var activeI=false;
+    
+    var strArr = [];
+    var newStr = [];
+    
+    text.split("").forEach(e => {
+      strArr.push(e);
+    });
+    
+    strArr.forEach(e => {
+      if (e != "*" && e != "_") {
+        newStr.push(e);
+      }
+      else {
+        if (activeB) {
+          newStr.push("</b>");
+          activeB = false;
+        }
+        else {
+          newStr.push("<b>");
+          activeB = true;
+        }
+
+        if (activeI) {
+          newStr.push("</i>");
+          activeI = false;
+        }
+        else {
+          newStr.push("<i>");
+          activeI = true;
+        }
+      }
+    });
+
+    return newStr.join("");
+  }
+}
